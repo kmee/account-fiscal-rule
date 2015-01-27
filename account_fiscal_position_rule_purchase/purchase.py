67cf3495b4c7a703df436a04aaea70da1f0b7d16
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-01-27 14:20:25 -0200

    Fixed minor errors in view fiscal position template

--- account_fiscal_position_rule_purchase/purchase.py
+++ account_fiscal_position_rule_purchase/purchase.py
@@ -25,12 +25,10 @@ from openerp.osv import osv
 class purchase_order(osv.Model):
     _inherit = 'purchase.order'
 
-    def _fiscal_position_map(self, cr, uid, result, **kwargs):
-
-        if not kwargs.get('context', False):
-            kwargs['context'] = {}
-
-        kwargs['context'].update({'use_domain': ('use_purchase', '=', True)})
+    def _fiscal_position_map(self, cr, uid, result, **kwargs):  
+        kwargs['context'] = dict(kwargs['context'] or {})
+        
+        kwargs['context'].update({'use_domain': ('use_purchase', '=', True)})       
         fp_rule_obj = self.pool.get('account.fiscal.position.rule')
         return fp_rule_obj.apply_fiscal_mapping(cr, uid, result, **kwargs)
 
